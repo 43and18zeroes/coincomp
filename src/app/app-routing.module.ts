960a8d877d7f5118226867f961f75ebbf8d668b2
Christoph <c.wagner.germany@gmail.com>
2024-03-17 14:54:28 +0100

    2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { CalculatorComponent } from './calculator/calculator.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
