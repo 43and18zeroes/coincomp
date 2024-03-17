4852985ecaa1b1d6d52b14a6bf891f2dd7c0b9fb
Christoph <c.wagner.germany@gmail.com>
2024-03-17 15:22:55 +0100

    btc

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.scss'
+  styleUrl: './calculator.component.scss',
 })
 export class CalculatorComponent {
 
+  ngOnInit() {
+    this.getWebSocket();
+  }
+
+  getWebSocket() {
+    console.log("test")
+    let ws = new WebSocket('wss://stream.binance.com:9443/ws/btceur@trade');
+
+    ws.onmessage = (event) => {
+      console.log(event.data);
+    }
+  }
 }
